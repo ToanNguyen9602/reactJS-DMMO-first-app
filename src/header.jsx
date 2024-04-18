e0665d7b2b03a87a1ba17e99bb87ddeb91a36bcf
ToanNguyen9602 <toannguyen9602@gmail.com>
2024-04-18 22:19:47 +0700

    commit headerFile

---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+const lastName = 'Toan'
+
+const sum = (a, b) => a + b
+
+const element = (
+  <h1>
+    hello, world! I am {lastName}. I am {sum(10, 16)}
+  </h1>
+)
+
+//tên thuộc tính phải chuyển thành camelCase
+const element2 = <a href='google.com'>Google</a>
