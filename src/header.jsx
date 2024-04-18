61c3a1a1aa01cb154b69380536f4a70cae87f74e
ToanNguyen9oanNguyen9602 <toannguyen9602@gmail.com>
2024-04-18 23:45:37 +0700

    commit again

--- src/header.jsx
+++ src/header.jsx
@@ -8,5 +8,30 @@ const element = (
   </h1>
 )
 
-//tên thuộc tính phải chuyển thành camelCase
-const element2 = <a href='google.com'>Google</a>
+//tên thuộc tính phải chuyển thành camelCase - tức là viết hoa chữ cái từ tiếp theo cua thuộc tính vdu tabIndex className
+const element2 = <a href='google.com' tabIndex="11" className='123' id="the-a">Google</a>
+
+//thẻ k có nội dung có thể viết gọn
+const element3 = <h3></h3>
+//const element3 = </h3>
+
+const element4 = <img src="" />
+const element5 = <input type="text" />
+
+const element6 = <h1 className="greeting">Hello</h1>
+
+//Babel bien dich
+// eslint-disable-next-line no-undef
+const element7 = React.createElement(
+  'h1',
+  { className: 'greeting' },
+  'Hello'
+)
+
+const elemen = {
+  type: 'h1',
+  props: {
+    className: 'greeting',
+    children: 'Hello'
+  }
+}
