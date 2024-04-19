62ca063c3bf11e013aea31f2965e06076483857e
ToanNguyen9602 <toannguyen9602@gmail.com>
2024-04-19 14:54:21 +0700

    commit video 77

--- src/index.js
+++ src/index.js
@@ -3,28 +3,39 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import Welcome from './Welcome'
 
 //const element = <h1>Hello World!</h1>
+//const root = ReactDOM.createRoot(document.getElementById('root'))
+// function tick() {
+//     const element = (
+//       <div>
+//         <h1>Hello World!</h1>
+//         <h2>It is {new Date().toLocaleTimeString()}</h2>
+//       </div>
+//     )
+//     root.render(element)
+//   }
+
+//   setInterval(tick,1000)
+
+const element = <Welcome name='Toan Nguyen' />
 const root = ReactDOM.createRoot(document.getElementById('root'))
-function tick() {
-    const element = (
-      <div>
-        <h1>Hello World!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}</h2>
-      </div>
-    )
-    root.render(element)
-  }
 
-  setInterval(tick,1000)
+const element2 = (
+  <div>
+    <Welcome name='Huy' age={26} />
+    <Welcome name='To' age={25} />
+    <Welcome name='Shin' age={2} />
+  </div>
+)
 
- //const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(
-//   element
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-//)
+root.render(
+  element2
+  //   <React.StrictMode>
+  //     <App />
+  //   </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
